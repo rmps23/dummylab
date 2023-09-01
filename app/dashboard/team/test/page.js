b858b6570149b8b89fe0c3491add48334015ad3b
rmps23 <rubensousa96@hotmail.com>
2023-09-01 23:47:26 +0100

    DummyLab - v0.38.9

--- app/dashboard/team/test/page.js
+++ app/dashboard/team/test/page.js
@@ -4,26 +4,44 @@ import { useState, useEffect } from "react";
 
 export default function Players() {
   const [data, setData] = useState(null);
+  const [stop, setStop] = useState(false);
 
   const fetchData = async () => {
     try {
       const response = await fetch(
         "https://127.0.0.1:2999/liveclientdata/allgamedata"
       );
+      if (!response.ok) {
+        // Handle non-okay responses here
+        throw new Error("Failed to fetch data");
+      }
       const jsonData = await response.json();
+
+      console.log(jsonData);
+
       setData(jsonData);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      //   console.error("Error fetching data:", error);
+      setStop(true); // Set stop to true on a failed response
     }
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (!stop) {
+      fetchData();
+
+      const intervalId = setInterval(fetchData, 2000);
+      return () => clearInterval(intervalId);
+    }
+  }, [stop]); // Include 'stop' as a dependency for useEffect
 
   return (
     <div>
-      <pre>{JSON.stringify(data)}</pre>
+      {data.activePlayer.summonerName}
+      <br />
+      {data.activePlayer.currentGold}
+      <br />
+      {data.gameData.gameTime}
     </div>
   );
 }
