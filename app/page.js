b223059a44a8f9278adb95d0dac9ebb3ca61306a
rmps23 <rubensousa96@hotmail.com>
2023-12-25 23:57:57 +0000

    DL v.0.1

--- app/page.js
+++ app/page.js
@@ -1,10 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import Signup from "../components/Auth/Signup";
-import Hero from "../components/Homepage/Hero";
+import { RiLoginCircleLine } from "react-icons/ri";
 
 const Home = () => {
   return (
     <>
-      <section
+      <section className="homepage-bg">
+        <div className="h-screen max-w-[1240px] px-5 py-8 mx-auto flex justify-between">
+          <div>
+            <Image
+              src={"/assets/dummylab-logo.svg"}
+              height={0}
+              width={100}
+            ></Image>
+          </div>
+          <div>
+            <Link
+              href={"/login"}
+              className="text-teal-500 text-sm font-medium flex items-center"
+            >
+              <span className="pt-1">LOGIN </span>
+              <RiLoginCircleLine className="text-xl ml-1" />
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="px-5 py-8 mx-auto mt-24 bg-zinc-800">
+        <div className="max-w-[1240px] flex justify-between mx-auto">
+          <div>
+            <p className="text-5xl font-bold italic">ELEVATE YOUR GAME</p>
+            <p className="font-thin text-lg max-w-lg text-zinc-300 leading-[20px] mt-4">
+              Guide and Enhance Your Team's Performance with the Ultimate
+              Coaching Experience!
+            </p>
+          </div>
+          <div>
+            <Signup></Signup>
+          </div>
+        </div>
+      </section>
+
+      {/* <section
         id="hero"
         className="items-center justify-center h-screen flex bg-gradient-to-br from-zinc-950 from-80%  to-teal-600 to-20% "
       >
@@ -17,7 +56,7 @@ const Home = () => {
             <Signup />
           </div>
         </div>
-      </section>
+      </section> */}
     </>
   );
 };
